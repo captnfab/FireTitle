4f226fb83e9c95c87d1a9232226f365f0b7368f1
Fabien Givors <fabien.givors@chezlefab.net>
2018-05-25 14:44:40 +0200

    tabindexes & focus hack

--- options/popup.js
+++ options/popup.js
@@ -7,3 +7,10 @@ document.addEventListener("DOMContentLoaded", fillFormWithCurWinSessionOptions);
 /* Form actions */
 document.querySelector("#set-pattern-as-default").addEventListener("click", setCurPatternAsDefault);
 document.querySelector("#apply-pattern-to-others").addEventListener("click", setCurPatternForOthers);
+
+function focusHack()
+{
+  setTimeout(function() { document.getElementById("cur_win_name").focus();}, 100);
+}
+
+document.addEventListener("DOMContentLoaded", focusHack);
